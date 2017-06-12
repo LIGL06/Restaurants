51487a3ce78a4cf1779bdbd8285fe04933be3287
danxmc <danx_97@hotmail.com>
2017-06-12 18:40:42 -0500

    Added restaurant model

---
+++ models/restaurant.js
@@ -0,0 +1,20 @@
+var mongoos = require('mongoose');
+var Schema = require.Schema;
+var mongoUrl = process.env.MONGODB_URI || 'mogodb://localhost/restaurants';
+mongoose.connect(mongoUri, function(error) {
+	if (error) throw error;
+})
+
+var restaurant_schema = new Schema({
+	name: String,
+	score: Number,
+	address: String,
+	visits: Number,
+	type: String,
+	created: {
+		type: Date,
+		default: Date.now
+	}
+});
+var Restaurant = mongoose.model('restaurant', restaurant_schema);
+modeule.exports.Restaurant = Restaurant;
