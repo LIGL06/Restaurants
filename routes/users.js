940965bd20cb346ce7b6a999b26f05d1357bc335
Luis Iván García Luna <lgarcia@vertixit.com>
2017-07-23 16:55:17 -0500

    Little fix

--- routes/users.js
+++ routes/users.js
@@ -4,37 +4,12 @@ var User = require('../models/user').User;
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:username', function(req, res, next) {
+  res.render('layout', { title: `Platzigram - ${req.params.username}` });  
 });
 
-router.post('/signup', function (req, res, next){
-  const user = new User({
-    fname: req.body.fname,
-    email: req.body.email,
-    username: req.body.username,
-    password: bcrypt.hashSync(req.body.password, 10)
-  });
-  user.save().then(function(){
-    res.send(user);
-  });
-});
-
-router.post('/signin', function(req, res, next){
-  if(req.body.hasOwnProperty('username') && req.body.hasOwnProperty('password')){
-    User.findOne({username: req.body.username}, function(error, user){
-      if(error) throw error;
-      if(!user) res.redirect('/auth/signup');
-      else{
-        if(bcrypt.compareSync(req.body.password, user.password)){
-          req.session.user_id = user._id
-          req.session.user_username = user.username
-          req.session.user_fname = user.fname
-        }
-        res.redirect('/');
-      }
-    });
-  }
+router.get('/:username/:id', function(req, res, next) {
+  res.render('layout', { title: `Platzigram - ${req.params.username}` });  
 });
 
 module.exports = router;
